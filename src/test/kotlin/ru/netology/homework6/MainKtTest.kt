ad3c541ab37f1500138f3c8d378d610f120aad47
Георгий Сагитов <georgmc@yandex.ru>
2022-08-25 19:38:59 +0300

    Add all files

---
+++ src/test/kotlin/ru/netology/homework6/MainKtTest.kt
@@ -0,0 +1,74 @@
+package ru.netology.homework6
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+
+class MainKtTest {
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+
+    }
+
+
+    @Test
+    fun add() {
+        val like = Likes()
+
+        val post = Post()
+        val post3 = post.copy(
+            ownerID = 32, data = System.currentTimeMillis(),
+            text = "Перерыв 10 минут!", friendsOnly = true,
+            likes = like.copy(count = like.count + 141, canPublish = true)
+        )
+        WallService.add(post3)
+        val array = WallService.getAll()
+        assertEquals(1, array[0].id)
+    }
+
+    @Test
+    fun updateTrue() {
+        val like = Likes()
+
+        val post = Post()
+        val post3 = post.copy(
+            text = "Перерыв 10 минут!", friendsOnly = true,
+            likes = like.copy(count = like.count + 141, canPublish = true)
+        )
+
+        val updatePost = post.copy(
+            1,
+            isFavorite = true, replyPostID = 2, replyOwnerID = 32,
+            canEdit = true,
+            text = "Перерыв 20 минут!", friendsOnly = true, postType = "suggest",
+            likes = like.copy(count = like.count + 151, canPublish = true)
+        )
+        WallService.add(post3)
+        val result = WallService.update(updatePost)
+        assertTrue(result)
+    }
+
+    @Test
+    fun updateFalse() {
+        val like = Likes()
+
+        val post = Post()
+        val post3 = post.copy(
+            text = "Перерыв 10 минут!", friendsOnly = true,
+            likes = like.copy(count = like.count + 141, canPublish = true)
+        )
+        WallService.add(post3)
+        val updatePost = post.copy(id=2,
+            isFavorite = true, replyPostID = 2, replyOwnerID = 32,
+            canEdit = true,
+            text = "Перерыв 20 минут!", friendsOnly = true, postType = "suggest",
+            likes = like.copy(count = like.count + 151, canPublish = true)
+        )
+
+        val result = WallService.update(updatePost)
+        assertFalse(result)
+    }
+}
